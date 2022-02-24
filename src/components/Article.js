db2338839dccf95501380efc60c6a9394199b8ee
Circle Ci <rogierderuijter@hotmail.com>
2022-02-24 18:49:48 +0100

    feat: add vpro cinema to the article

---
+++ src/components/Article.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Container = styled.article`
+  background-color: gray;
+  aspect-ratio: 325/216;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  justify-content: ${(props) => props.cinema && "flex-end"};
+  background-image: url(${(props) => props.backgroundImg});
+  background-size: contain;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  position: relative;
+`;
+
+const VideoBanner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 35px;
+  width: 100%;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  padding-left: 5%;
+`;
+
+function Article({ recommendation }) {
+  return (
+    <Container
+      backgroundImg={recommendation.img}
+      cinema={recommendation.cinema}
+    >
+      {recommendation.cinema && <VideoBanner>VPRO Cinema</VideoBanner>}
+      {!recommendation.cinema && <aside>{recommendation.additionalInfo}</aside>}
+      <header>{recommendation.title}</header>
+    </Container>
+  );
+}
+
+Article.propTypes = {
+  recommendation: PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    additionalInfo: PropTypes.string,
+    cinema: PropTypes.bool,
+  }).isRequired,
+};
+
+export default Article;
